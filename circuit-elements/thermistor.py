dfe535528f905f03aaa7801dc3f17e5f1710ee8f
p-zavyalova <p.zavyalova@yahoo.com>
2018-01-14 19:30:25 -0500

    added observations

--- circuit-elements/thermistor.py
+++ circuit-elements/thermistor.py
@@ -5,18 +5,28 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 # ---------- User-Defined Functions ----------
 
+# Fit function for optimizing; 
 def f(x, a, b):
-	
+	return a * np.exp(- x / b)
 
 # ---------- Main Code ----------
 
 path_to_file = "/home/polina/Documents/3rd_Year/PHY324/circuit-elements/thermistor.txt"
 temp, resist = np.loadtxt(path_to_file, unpack = True)
+temp = temp + 273.0			# Convert temperature to K
 
+print(temp)
+print(resist)
 
+# Arrays w/ uncertainties
+temp_unc = np.full(np.size(temp), 0.5)
+resist_unc = np.concatenate((np.full(12, 0.01), np.full(18, 0.1)))
+
+popt, pcov = curve_fit(f, temp, resist)
 
 plt.plot(temp, resist)
 plt.grid(True)
