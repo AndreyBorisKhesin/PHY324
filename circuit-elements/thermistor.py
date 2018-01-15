ed4e63724bc94beabcdc9fa15fdb954c294dfb51
p-zavyalova <p.zavyalova@yahoo.com>
2018-01-15 12:45:04 -0500

    Changed .py for fitting

--- circuit-elements/thermistor.py
+++ circuit-elements/thermistor.py
@@ -9,28 +9,34 @@ from scipy.optimize import curve_fit
 
 # ---------- User-Defined Functions ----------
 
-# Fit function for optimizing; 
+# Fit function for optimizing; a = R_0, b = T_0
+# (x, a, b) --> (f_value)
+# (float, float, float) --> (float)
 def f(x, a, b):
 	return a * np.exp(- x / b)
 
 # ---------- Main Code ----------
 
 path_to_file = "/home/polina/Documents/3rd_Year/PHY324/circuit-elements/thermistor.txt"
-temp, resist = np.loadtxt(path_to_file, unpack = True)
-temp = temp + 273.0			# Convert temperature to K
+# Load data
+temp, temp_unc, resist, resist_unc = np.loadtxt(path_to_file, unpack = True)
+# Convert data to Kelvin, kiloohms
+temp = temp + 273.0
+temp_unc = temp_unc + 273.0
+ddof = np.size(temp) - 2	# degrees of freedom for reduced chi squared
+print(ddof)
 
-print(temp)
-print(resist)
-
-# Arrays w/ uncertainties
-temp_unc = np.full(np.size(temp), 0.5)
-resist_unc = np.concatenate((np.full(12, 0.01), np.full(18, 0.1)))
-
-popt, pcov = curve_fit(f, temp, resist)
+popt, pcov = curve_fit(f, temp, resist, sigma = resist_unc, p0 = [1e6, 10])
+print(popt)
+# Find residuals
+r = temp - f(temp, *popt)
+chisq = np.sum((r / resist_unc) ** 2)
+print "Reduced chi squared:", chisq / ddof
 
 plt.plot(temp, resist)
+plt.plot(temp, f(temp, *popt))
 plt.grid(True)
-plt.xlabel("Temperature (C)")
-plt.ylabel("Resistance (ohms)")
+plt.xlabel("Temperature (K)")
+plt.ylabel("Resistance (kiloohms)")
 plt.title("Resistance vs. Temperature for a Thermistor")
 plt.show()
