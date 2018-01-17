06b6aa0f31ba3c676efd8268b0edd6124efbc235
p-zavyalova <p.zavyalova@yahoo.com>
2018-01-17 16:05:06 -0500

    Fixed based on comments

--- circuit-elements/bulb.py
+++ circuit-elements/bulb.py
@@ -17,6 +17,8 @@ def f(x, k, a):
 
 # ---------- Main Code ----------
 
+# Set desired font
+plt.rc('font', family = 'Times New Roman')
 path_to_file = "/home/polina/Documents/3rd_Year/PHY324/circuit-elements/bulb.txt"
 # Load data
 V, V_unc, I, I_unc = np.loadtxt(path_to_file, unpack = True)
@@ -32,6 +34,7 @@ I_unc = I_unc[n:]
 ddof = np.size(V) - 2	# degrees of freedom for reduced chi squared
 print(ddof)
 
+# Fit data to model function
 popt, pcov = curve_fit(f, I, V, sigma = V_unc, p0 = [1, 1])
 print(popt)
 print "k:", popt[0], "+-", np.sqrt(pcov[0, 0])
@@ -41,14 +44,20 @@ r = V - f(I, *popt)
 chisq = np.sum((r / V_unc) ** 2)
 print "Reduced chi squared:", chisq / ddof
 
+# Calculate and print R^2
+ss_res = np.sum(r ** 2)
+ss_tot = np.sum((V - np.mean(V)) ** 2)
+r_squared = 1 - (ss_res / ss_tot)
+print "R^2:", r_squared
+
 fig1 = plt.figure(1)
 # Plot data + model
 frame1 = fig1.add_axes((0.1, 0.3, 0.8, 0.6))
 plt.scatter(I, V, label = "Data", s = 10, color = "black")
 plt.errorbar(I, V, xerr = I_unc, yerr = V_unc, linestyle = "None", color = "black")
-plt.plot(I, f(I, *popt), label = "Model")
+plt.plot(I, f(I, *popt), label = "Model", color = "black")
 plt.title("Potential vs. Current of a Light Bulb")
-plt.legend()
+# plt.legend()
 plt.ylabel("Potential (V)")
 plt.xlim([-5, 45])
 plt.ylim([-4, 29])
--- circuit-elements/thermistor.py
+++ circuit-elements/thermistor.py
@@ -17,6 +17,8 @@ def f(x, a, b):
 
 # ---------- Main Code ----------
 
+# Set desired font
+plt.rc('font', family = 'Times New Roman')
 path_to_file = "/home/polina/Documents/3rd_Year/PHY324/circuit-elements/thermistor.txt"
 # Load data
 temp, temp_unc, resist, resist_unc = np.loadtxt(path_to_file, unpack = True)
@@ -32,8 +34,8 @@ resist = resist[:np.size(resist) - n]
 resist_unc = resist_unc[:np.size(resist_unc) - n]
 
 ddof = np.size(temp) - 2	# degrees of freedom for reduced chi squared
-print(ddof)
 
+# Fit data to model function
 popt, pcov = curve_fit(f, temp, resist, sigma = resist_unc, p0 = [1e6, 10])
 print(popt)
 print "R_0:", popt[0], "+-", np.sqrt(pcov[0, 0])
@@ -43,14 +45,20 @@ r = resist - f(temp, *popt)
 chisq = np.sum((r / resist_unc) ** 2)
 print "Reduced chi squared:", chisq / ddof
 
+# Calculate and print R^2
+ss_res = np.sum(r ** 2)
+ss_tot = np.sum((resist - np.mean(resist)) ** 2)
+r_squared = 1 - (ss_res / ss_tot)
+print "R^2:", r_squared
+
 fig1 = plt.figure(1)
 # Plot data + model
 frame1 = fig1.add_axes((0.1, 0.3, 0.8, 0.6))
 plt.scatter(temp, resist, label = "Data", s = 10, color = "black")
 plt.errorbar(temp, resist, xerr = temp_unc, yerr = resist_unc, linestyle = "None", color = "black")
-plt.plot(temp, f(temp, *popt), label = "Model")
+plt.plot(temp, f(temp, *popt), label = "Model", color = "black")
 plt.title("Resistance vs. Temperature of a Thermistor")
-plt.legend()
+# plt.legend()
 plt.ylabel("Resistance (kiloohms)")
 plt.xlim([270, 360])
 # frame1.axes().get_xaxis().set_visible(False)
