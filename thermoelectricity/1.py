13ea4c1cb6b12e30566d5fe09cba50632d52e212
p-zavyalova <p.zavyalova@yahoo.com>
2018-02-12 23:41:51 -0500

    Residuals

--- thermoelectricity/1.py
+++ thermoelectricity/1.py
@@ -33,11 +33,11 @@ popt1, pcov1 = curve_fit(func, P, T2 - T1)
 print "a:", popt1[0], "+-", np.sqrt(pcov1[0, 0])
 print "b:", popt1[1], "+-", np.sqrt(pcov1[1, 1])
 # Find residuals
-r = (T2 - T1) - func(P, *popt1)
-chisq = np.sum((r / T_unc) ** 2)
+r1 = (T2 - T1) - func(P, *popt1)
+chisq = np.sum((r1 / T_unc) ** 2)
 print "Reduced chi squared:", chisq / ddof
 # Calculate and print R^2
-ss_res = np.sum(r ** 2)
+ss_res = np.sum(r1 ** 2)
 ss_tot = np.sum((T2 - T1 - np.mean(T2 - T1)) ** 2)
 r_squared = 1 - (ss_res / ss_tot)
 print "R^2:", r_squared
@@ -50,17 +50,19 @@ print "a:", popt2[0], "+-", np.sqrt(pcov2[0, 0])
 print "b:", popt2[1], "+-", np.sqrt(pcov2[1, 1])
 
 # Find residuals
-r = (T1 - T0) - func(P, *popt2)
-chisq = np.sum((r / T_unc) ** 2)
+r2 = (T1 - T0) - func(P, *popt2)
+chisq = np.sum((r2 / T_unc) ** 2)
 print "Reduced chi squared:", chisq / ddof
 # Calculate and print R^2
-ss_res = np.sum(r ** 2)
+ss_res = np.sum(r2 ** 2)
 ss_tot = np.sum((T1 - T0 - np.mean(T1 - T0)) ** 2)
 r_squared = 1 - (ss_res / ss_tot)
 print "R^2:", r_squared
 
 print "\n K_hs", 1 / popt2[0], "+-", np.sqrt(pcov2[0, 0]) / (popt2[0] ** 2), "\n"
 
+fig1 = plt.figure(1)
+frame1 = fig1.add_axes((0.1, 0.3, 0.8, 0.6))
 plt.plot(P, T2 - T1, label = "T_in - T_out", color = "red")
 plt.errorbar(P, T2 - T1, xerr = P_unc, yerr = T_unc, linestyle = "None", color = "black")
 plt.plot(P, func(P, *popt1))
@@ -69,8 +71,15 @@ plt.errorbar(P, T1 - T0, xerr = P_unc, yerr = T_unc, linestyle = "None", color =
 plt.plot(P, func(P, *popt2))
 plt.grid()
 plt.title("Temperature difference vs. input power")
-plt.xlabel("Power (W)")
 plt.ylabel("Temperature (Degrees)")
+plt.xlim([-0.2, 5.2])
 plt.legend()
+frame2 = fig1.add_axes((0.1, 0.1, 0.8, 0.2))
+plt.scatter(P, r1, color = "red")
+plt.scatter(P, r2, color = "green")
+plt.xlabel("Power (W)")
+plt.ylabel("Residuals")
+plt.xlim([-0.2, 5.2])
+plt.grid(True)
 plt.savefig("1.pdf")
 plt.show()
